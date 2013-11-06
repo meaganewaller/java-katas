99c80e681d4e28e31e5943ef32d00ef123937edd
Meagan Waller <meaganewaller@gmail.com>
2013-11-06 16:40:43 -0600

    Adds Second Kata Implementation

---
+++ src/com/kata/junit/PrimeFactorsTwo.java
@@ -0,0 +1,11 @@
+package com.kata.junit;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: meaganewaller
+ * Date: 11/6/13
+ * Time: 4:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PrimeFactorsTwo {
+}
---
+++ test/com/kata/junit/PrimeFactorsTwoTest.java
@@ -0,0 +1,19 @@
+package com.kata.junit;
+
+import org.junit.Before;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: meaganewaller
+ * Date: 11/6/13
+ * Time: 4:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PrimeFactorsTwoTest {
+    private PrimeFactorsTwo primeFactors;
+
+    @Before
+    public void setUp() throws Exception {
+        primeFactors = new PrimeFactorsTwo();
+    }
+}
